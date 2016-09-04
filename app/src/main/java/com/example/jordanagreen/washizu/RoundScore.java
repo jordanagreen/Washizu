4aa7b9bede882e62ac1eb403178e5133bc486526
Jordan Green <thegreenjordan@gmail.com>
2016-09-03 23:31:29 -0400

    Create class for keeping track of score changes after every round

---
+++ app/src/main/java/com/example/jordanagreen/washizu/RoundScore.java
@@ -0,0 +1,30 @@
+package com.example.jordanagreen.washizu;
+
+/**
+ * Created by Jordan on 9/3/2016.
+ */
+public class RoundScore {
+
+    private static final String COLOR_STRING = "<font color='%s'>%s</font>";
+    private static final String RED = "#FF0000";
+    private static final String BLUE = "#00FF00";
+
+    private int scoreChange;
+    private String scoreColor;
+
+    public RoundScore(int scoreChange){
+        this.scoreChange = scoreChange;
+        if (scoreChange > 0){
+            scoreColor = BLUE;
+        } else if (scoreChange < 0){
+            scoreColor = RED;
+        }
+    }
+
+    public String getScoreString(){
+        if (scoreChange == 0) return scoreChange + "";
+        String sign = (scoreChange > 0) ? "+" : "-";
+        return String.format(COLOR_STRING, scoreColor, sign + Math.abs(scoreChange));
+    }
+
+}
---
+++ app/src/test/java/com/example/jordanagreen/washizu/test/RoundScoreTest.java
@@ -0,0 +1,34 @@
+package com.example.jordanagreen.washizu.test;
+
+
+import com.example.jordanagreen.washizu.RoundScore;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Jordan on 9/3/2016.
+ */
+public class RoundScoreTest {
+
+    private RoundScore roundScore;
+
+    @Test
+    public void testNoChange(){
+        roundScore = new RoundScore(0);
+        assertEquals(roundScore.getScoreString(), "0");
+    }
+
+    @Test
+    public void testIncrease(){
+        roundScore = new RoundScore(500);
+        assertEquals(roundScore.getScoreString(), "<font color='#00FF00'>+500</font>");
+    }
+
+    @Test
+    public void testDecrease(){
+        roundScore = new RoundScore(-500);
+        assertEquals(roundScore.getScoreString(), "<font color='#FF0000'>-500</font>");
+    }
+}
